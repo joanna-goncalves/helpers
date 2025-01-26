82ea015d65c69fb158279e1173927048177bb20f
joanna-goncalves <joanna.goncalvesp@gmail.com>
2025-01-26 12:54:40 +0100

    first push

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+requirements = ["toml", "PyYAML"]
+
+setup(
+    name="helpers",
+    version="1.0",
+    install_requires=requirements,
+    packages=find_packages(),
+    author="your_name",
+)
+
+
+
